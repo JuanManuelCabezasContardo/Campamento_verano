befac105817992378d7a151bc3db26a91013930d
JuanManuelCabezasContardo <juanmanuelcontardo@gmail.com>
2021-02-02 11:44:59 -0300

    agregada la bd

--- bd/database.sql
+++ bd/database.sql
@@ -1,13 +1,29 @@
-drop table user;
-drop table modulo;
-drop table asistentes;-- asistentes - profesores
-drop table clase;
-drop table horario; -- relacion triple (asistente - modulo - clase)
 
 
-
-create table user();
-create table modulo();
-create table asistentes();-- asistentes - profesores
-create table clase();
-create table horario(); -- relacion triple (asistente - modulo - clase)
+create table users(
+    id int primary key,
+    password varchar(25),
+    nombre varchar(24)
+),
+create table modulo(
+    id int primary key,
+    hora_ini varchar(25),
+    hora_fin varchar(25)
+),
+create table asistentes(
+    id int primary key,
+    nombre varchar(30)
+),
+create table clase(
+    id int primary key,
+    nombre_clase varchar(29)
+),
+create table horario(
+    id_modulo int,
+    id_asistente int,
+    id_clase int,
+    primary key (id_modulo,id_asistente,id_clase),
+    CONSTRAINT FK_modulo foreign key (id_modulo) references modulo(id),
+    CONSTRAINT FK_asistente foreign key (id_asistente) references asistentes(id),
+    CONSTRAINT FK_clase foreign key (id_clase) references clase(id)
+); -- relacion triple (asistente - modulo - clase)

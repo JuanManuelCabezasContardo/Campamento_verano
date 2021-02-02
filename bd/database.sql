4c914716efc3f06874ae9ffd3c88d0c01b5a5298
JuanManuelCabezasContardo <juanmanuelcontardo@gmail.com>
2021-02-02 11:12:19 -0300

    commit 2 dia campamento

---
+++ bd/database.sql
@@ -0,0 +1,13 @@
+drop table user;
+drop table modulo;
+drop table asistentes;-- asistentes - profesores
+drop table clase;
+drop table horario; -- relacion triple (asistente - modulo - clase)
+
+
+
+create table user();
+create table modulo();
+create table asistentes();-- asistentes - profesores
+create table clase();
+create table horario(); -- relacion triple (asistente - modulo - clase)
